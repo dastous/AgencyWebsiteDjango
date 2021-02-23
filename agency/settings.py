3b7c5cb5e49c53fd45767e3512729a95452331c7
Louis-Philippe Préfontaine-Dastous <louis.prefontaine.dastous@gmail.com>
2021-02-23 17:31:05 -0500

    Update settings.py

--- agency/settings.py
+++ agency/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xynz%k5+c0n5vax43tf^r7!x@7ep*ne)&c!1@qg%(bdn3b4gph'
+SECRET_KEY = '**********'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
