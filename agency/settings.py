0bdfdd2bbfc291399524f3c21903d16f763ad09f
Louis-Philippe Préfontaine-Dastous <louis.prefontaine.dastous@gmail.com>
2021-02-23 17:22:58 -0500

    Update settings.py

--- agency/settings.py
+++ agency/settings.py
@@ -129,6 +129,6 @@ STATICFILES_DIRS = [
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'louis.prefontaine.dastous@gmail.com'
-EMAIL_HOST_PASSWORD = 'iqxi jwxi syfv bbiq'
+EMAIL_HOST_PASSWORD = '**** **** **** ****'
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = FALSE
